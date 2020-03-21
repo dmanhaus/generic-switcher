9c6412c87bb6bfd2dfecc2f35d05a0a570ee495f
Dave Hausler <dave.hausler@gmail.com>
2020-03-21 18:34:17 -0400

    added switch method to SwitcherBaseClass
    The purpose of the switch method is to exactly match the string
    passed to the 'argument' parameter to the name of a function
    that exists in the class that uses the SwitchBaseClass as its subclass.
    
    Since the switch method is always the same syntax, no matter
    what class calls it, I replaced the original abstract switch function
    with this one.

--- switcher/switcher.py
+++ switcher/switcher.py
@@ -1,10 +1,16 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 
 class SwitcherBaseClass(metaclass=ABCMeta):
   def __init__(self, name=None):
     if name:
       self.name = name
 
-  @abstractmethod
-  def switch(self):
-    pass
+  def switch(self, argument):
+    method_name = str(argument)
+    # Get the method from 'self'. Default to a lambda
+
+    method = getattr(self, method_name, lambda: "Invalid request")
+
+    # Call the method as we return it
+
+    return method()
