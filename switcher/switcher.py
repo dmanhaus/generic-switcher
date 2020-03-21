663c847bca187137e5d703c523aec110a2b80442
Dave Hausler <dave.hausler@gmail.com>
2020-03-21 13:38:57 -0400

    renamed abstractmethod 'base_switch' to 'switch'

--- switcher/switcher.py
+++ switcher/switcher.py
@@ -6,11 +6,5 @@ class SwitcherBaseClass(metaclass=ABCMeta):
       self.name = name
 
   @abstractmethod
-  def base_switch(self, argument):
-    # Get the method from 'self'. Default to a lambda
-
-    method = getattr(self, argument, lambda: "Invalid request")
-
-    # Call the method as we return it
-
-    return method()
+  def switch(self):
+    pass
